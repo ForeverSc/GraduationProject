587c5aed3d368cccf1a9d761f83ccaf7ef87f18a
ForeverSc <fengshuochn@126.com>
2017-08-07 21:45:52 +0800

    add two file

---
+++ pc-frontend/src/components/DatePicker.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <div class="vsearch-container">
+        <div class="vsearch-line">1</div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name: 'date-picker',
+    props: ['value']
+}
+</script>
+<style>
+.vsearch-container {
+    width: 100%;
+    background: #ccc;
+}
+
+.vsearch-line {
+    height: 48px;
+    line-height: 48px;
+    border: 1px dashed #aac;
+}
+</style>
---
+++ pc-frontend/src/components/addressSelector.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+    <el-select v-model="value.value1">
+        <el-option v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+        </el-option>
+    </el-select>
+    <el-select v-model="value.value2">
+        <el-option v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+        </el-option>
+    </el-select>
+</div>
+</template>
+<script>
+export default {
+    name: 'address-selector',
+    props: ['value'],
+    data() {
+        return {
+             options: [{
+                value: '1',
+                label: '黄金糕'
+                }, {
+                value: '2',
+                label: '双皮奶'
+                }, {
+                value: '3',
+                label: '蚵仔煎'
+                }, {
+                value: '4',
+                label: '龙须面'
+                }, {
+                value: '5',
+                label: '北京烤鸭'
+            }]
+        }
+    },
+    watch: {
+        value(val){
+            console.log(val)
+            this.$emit('input',val)
+        }
+    }
+}  
+</script>
